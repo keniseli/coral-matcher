9aca6dc1a2100774a70d508e361abed5b2d50e65
Ken Iseli <ken.iseli@gmail.com>
2026-08-03 22:39:20 -0600

    💅 display monitoring session summary as more important than coral name

--- frontend/src/components/monitoring/ObservationSummaryRow.vue
+++ frontend/src/components/monitoring/ObservationSummaryRow.vue
@@ -3,18 +3,18 @@
         <div class="flex-col shrink-0 w-monitoring-metric-label-column">
             &nbsp;
         </div>
-        <div v-for="observation in observations" class="flex w-monitoring-metric-value-column shrink-0 flex-col
-        rounded-xs border border-coral-surface-border bg-coral-surface p-3">
+        <div v-for="observation in observations" class="flex w-monitoring-metric-value-column 
+            shrink-0 flex-col p-3">
 
             <img src="https://storage.googleapis.com/coral-matcher-media/Isla larga/c004/cropped_2026-07-24 20:28:02.795031"
                 class="mb-3 rounded object-cover" />
 
-            <div class="font-semibold text-xs">
-                {{ observation.coralName }}
-            </div>
-
-            <div class="text-xs text-coral-secondary-text">
+            <div class="font-semibold text-xs text-center">
                 {{ observation.monitoringSessionSummary }}
+            </div>
+            
+            <div class="text-xs text-coral-secondary-text text-center">
+                {{ observation.coralName }}
             </div>
 
         </div>
